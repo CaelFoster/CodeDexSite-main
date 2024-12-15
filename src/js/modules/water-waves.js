00b0414a38e87c06aac7be90eace5f9f45e4d187
Cael Foster <SlenderZX1@gmail.com>
2024-12-15 14:48:53 -0800

    added a water animation

---
+++ src/js/modules/water-waves.js
@@ -0,0 +1,12 @@
+// https://codepen.io/creativeocean/pen/ZEEOxmK
+VANTA.FOG({
+  el: "#water",
+  highlightColor: 0xfff,
+  midtoneColor: 0xfff,
+  lowlightColor: 0x1ee,
+  baseColor: 0x00f,
+  blurFactor: 0.2,
+  speed: 0.23,
+  zoom: 0.25
+})
+
